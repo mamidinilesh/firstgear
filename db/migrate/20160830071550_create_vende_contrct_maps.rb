870a8fed9d0befb0c0092726677cf592b62fbcf4
Nilesh Mamidi <mamidi.nilesh@gmail.com>
2016-08-30 12:47:35 +0530

    migration sequence change

--- db/migrate/20160830071550_create_vende_contrct_maps.rb
+++ db/migrate/20160830071550_create_vende_contrct_maps.rb
@@ -2,12 +2,12 @@ class CreateVendeContrctMaps < ActiveRecord::Migration[5.0]
   def change
     create_table :vende_contrct_maps do |t|
       t.references :vendor_contract, foreign_key: true
-      t.references :category, foreign_key: true
-      t.decimal :commission
-      t.boolean :is_inclusive
-      t.boolean :active
+     t.references :category, foreign_key: true
+     t.decimal :commission
+     t.boolean :is_inclusive
+     t.boolean :active
 
-      t.timestamps
+     t.timestamps
     end
   end
 end
